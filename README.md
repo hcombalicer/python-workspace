bc7f152f89138446cd7736cfc2e0296a0aea5ddb
Just Harold <40112059+hcombalicer@users.noreply.github.com>
2025-04-17 17:27:06 +0000

    updating based on precommit linter

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Python Codes for Google Workspace
 
 
-## Developing in codespace 
+## Developing in codespace
 1. git status
 2. git add . / git add <filename1> <filename1>
 3. git commit -m "Your concise commit message here"
--- sheets/main.py
+++ sheets/main.py
@@ -1,14 +1,14 @@
 """This is a testing module for connecting to google workspace"""
-import os
+
 import json
+import os
+
 import gspread
 from google.oauth2.service_account import Credentials
 
 api_key = os.environ.get("API_KEY")
 sheet_id = os.environ.get("TEST_SHEETID")
-scopes = [
-    "https://www.googleapis.com/auth/spreadsheets"
-]
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 service_account_info = json.loads(api_key)
 creds = Credentials.from_service_account_info(service_account_info, scopes=scopes)
 client = gspread.authorize(creds)
