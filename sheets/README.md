5eddc6f34e2d333d1f689386ee031d3cab3f7e12
Just Harold <40112059+hcombalicer@users.noreply.github.com>
2025-04-17 16:41:27 +0000

    testing googleworkspace API in python

---
+++ sheets/README.md
@@ -0,0 +1,7 @@
+# Setting up the program 
+1. Load the sheetid.txt and credentials.json
+2. Run the following commands to create local variables:  
+`FILE_SHEET=$(cat sheetid.txt)`  
+`export TEST_SHEETID="$FILE_SHEET"`  
+`FILE_CONTENT=$(cat credentials.json)`  
+`export API_KEY="$FILE_CONTENT"`
---
+++ sheets/main.py
@@ -0,0 +1,18 @@
+"""This is a testing module for connecting to google workspace"""
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+
+api_key = os.environ.get("API_KEY")
+sheet_id = os.environ.get("TEST_SHEETID")
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+service_account_info = json.loads(api_key)
+creds = Credentials.from_service_account_info(service_account_info, scopes=scopes)
+client = gspread.authorize(creds)
+sheet = client.open_by_key(sheet_id)
+
+values_list = sheet.sheet1.row_values(1)
+print(values_list)
